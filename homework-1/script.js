77ade02ac8b62e4245290bda404c4af1b31200a8
AndriyCh1 <chekanovskiyacc@gmail.com>
2023-11-21 14:30:18 +0200

    feat: homework 1 completed

---
+++ homework-1/script.js
@@ -0,0 +1,52 @@
+// ------------- Task 1
+let strHello = 'hello';
+let strOwu = 'owu';
+let strCom = 'com';
+let strUa = 'ua';
+let num1 = 1;
+let num10 = 10;
+let numNegative999 = -999;
+let num123 = 123;
+let numPi = 3.14;
+let num2_7 = 2.7;
+let num16 = 16;
+let booleanTrue = true;
+let booleanFalse = false;
+
+console.log(strHello);
+console.log(strOwu);
+console.log(strCom);
+console.log(strUa);
+console.log(num1);
+console.log(num10);
+console.log(numNegative999);
+console.log(num123);
+console.log(numPi);
+console.log(num2_7);
+console.log(num16);
+console.log(booleanTrue);
+console.log(booleanFalse);
+
+// ------------- Task 2
+let firstName = 'Alex';
+let middleName = 'Doe';
+let lastName = 'Smith';
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+// ------------- Task 3
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+// ------------- Task 4
+let yourName = prompt('Ім\'я:');
+let yourMiddleName = prompt('По-батькові:');
+let yourAge = prompt('Вік:');
+
+console.log('Ваше ім\'я: ' + yourName);
+console.log('Ваше по-батькові: ' + yourMiddleName);
+console.log('Ваш вік: ' + yourAge);
